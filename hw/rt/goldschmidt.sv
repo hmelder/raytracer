60f5ece0a85dfa0793a44f6342234194340b83fc
hmelder <service@hugomelder.com>
2025-04-08 12:46:46 +0800

    Change IW, QW parameters

--- hw/rt/goldschmidt.sv
+++ hw/rt/goldschmidt.sv
@@ -15,62 +15,51 @@ module goldschmidt (
 
   sfp_if #(IW, QW) const_3 ();
 
-  parameter IW = in.IW * 2;
-  parameter QW = in.QW * 2;
+  parameter IW = in.IW;  // * 2;
+  parameter QW = in.QW;  // * 2;
 
   assign const_3.val = 3 << IW;
 
   // --- Pipeline Registers ---
-  // Stage 0: Input Registers
-  sfp_if #(in.IW, in.QW) b_0_reg (), y_0_reg ();
+  // Stage -1: (Sorry this was bolted on later)
+  sfp_if #(IW, QW) b_0_reg (), y_0_reg ();
+
+  // Stage 0:
   sfp_if #(IW, QW) x_0_reg (), YY_0_reg ();
 
   // Stage 1 
-  sfp_if #(IW, QW) x_0_reg_stage1 (), b_1_reg ();
-  sfp_if #(in.IW, in.QW) y_0_reg_stage1 ();
+  sfp_if #(IW, QW) x_0_reg_stage1 (), y_0_reg_stage1 (), b_1_reg ();
 
   // Stage 2
-  sfp_if #(IW, QW) x_0_reg_stage2 (), b_1_reg_stage2 (), Y_1_reg ();
-  sfp_if #(in.IW, in.QW) y_0_reg_stage2 ();
+  sfp_if #(IW, QW) x_0_reg_stage2 (), y_0_reg_stage2 (), b_1_reg_stage2 (), Y_1_reg ();
 
   // Stage 3
   sfp_if #(IW, QW) YY_1_reg (), b_1_reg_stage3 ();
   sfp_if #(rsqrt.IW, rsqrt.QW) y_1_reg ();
   sfp_if #(sqrt.IW, sqrt.QW) x_1_reg ();
 
-  /*
-  // Stage 4
-  sfp_if #(IW, QW) b_2_reg (), x_1_reg_stage4 (), y_1_reg_stage4 ();
-
-  // Stage 5
-  sfp_if #(IW, QW) x_1_reg_stage5 (), y_1_reg_stage5 (), Y_2_reg ();
-
-  // Stage 6: Final multiplication (output register)
-  sfp_if #(IW, QW) x_2_reg (), y_2_reg ();
-  */
-
 
   // --- Pipeline Control ---
-  localparam PIPE_DEPTH = 4;
+  localparam PIPE_DEPTH = 5;
   logic [PIPE_DEPTH-1:0] pipe_valid;  // Shift register for valid signal
 
 
   // --- Combinational Logic for Pipeline Stages ---
   sfp_if #(IW, QW) tmp_x_0_stage0 (), tmp_YY_0_stage0 ();
 
-  // Stage 0 Logic (Inputs: in, est)
+  // Stage 0 Logic (Inputs: b_0_reg, y_0_reg)
   // x_0 = S * y_0 <=> in * est
   sfp_mul mul_x_0_stage0 (
-      .x(in),
-      .y(est),
+      .x(b_0_reg),
+      .y(y_0_reg),
       .out(tmp_x_0_stage0),
       .clipping()
   );
 
   // YY_0 = Y_0 * Y_0
   sfp_mul mul_YY_0_stage0 (
-      .x(est),
-      .y(est),
+      .x(y_0_reg),
+      .y(y_0_reg),
       .out(tmp_YY_0_stage0),
       .clipping()
   );
@@ -117,55 +106,6 @@ module goldschmidt (
       .clipping()
   );
 
-  /*
-
-  // YY_1 = Y_1 * Y_1
-  sfp_mul mul_YY_1_stage3 (
-      .x(Y_1_reg),
-      .y(Y_1_reg),
-      .out(tmp_YY_1_stage3),
-      .clipping()
-  );
-
-  // Stage 4 (Inputs: b_1_reg_stage3, YY_1_reg)
-  sfp_if #(IW, QW) tmp_b_2_stage4 ();
-
-  sfp_mul mul_b_2_stage4 (
-      .x(b_1_reg_stage3),
-      .y(YY_1_reg),
-      .out(tmp_b_2_stage4),
-      .clipping()
-  );
-
-  // Stage 5 (Inputs: b_2_reg)
-  sfp_if #(IW, QW) tmp_Y_2_stage5 ();
-
-  sfp_sub sub_Y_2_stage5 (
-      .in1(const_3),
-      .in2(b_2_reg),
-      .out(tmp_Y_2_stage5),
-      .clipping()
-  );
-
-  // Stage 6 (Inputs: x_1_reg_stage5, y_1_reg_stage5, Y_2_reg)
-  sfp_if #(IW, QW) tmp_x_2_stage6 (), tmp_y_2_stage6 ();
-
-  // x_2 = x_1 * Y_2
-  sfp_mul mul_x_2_stage5 (
-      .x(x_1_reg_stage5),
-      .y(Y_2_reg),
-      .out(tmp_x_2_stage6),
-      .clipping()
-  );
-  // y_1 = y_1 * Y_2
-  sfp_mul mul_y_2_stage5 (
-      .x(y_1_reg_stage5),
-      .y(Y_2_reg),
-      .out(tmp_y_2_stage6),
-      .clipping()
-  );
-  */
-
   // --- Sequential Logic (Registers and Control) ---
   always_ff @(posedge clk) begin
     if (!resetn) begin
@@ -176,23 +116,27 @@ module goldschmidt (
       pipe_valid <= {pipe_valid[PIPE_DEPTH-2:0], start};  // Shift valid bit
     end
 
-    // Stage 0
+    // Stage -1
     if (start) begin
-      b_0_reg.val  <= in.val;
+      b_0_reg.val <= in.val;
+      y_0_reg.val <= est.val;
+    end
+
+    // Stage 0
+    if (pipe_valid[0]) begin
       x_0_reg.val  <= tmp_x_0_stage0.val;
-      y_0_reg.val  <= est.val;
       YY_0_reg.val <= tmp_YY_0_stage0.val;
     end
 
     // Stage 1
-    if (pipe_valid[0]) begin
+    if (pipe_valid[1]) begin
       x_0_reg_stage1.val <= x_0_reg.val;
       y_0_reg_stage1.val <= y_0_reg.val;
       b_1_reg.val = tmp_b_1_stage1.val;
     end
 
     // Stage 2
-    if (pipe_valid[1]) begin
+    if (pipe_valid[2]) begin
       x_0_reg_stage2.val <= x_0_reg_stage1.val;
       y_0_reg_stage2.val <= y_0_reg_stage1.val;
       b_1_reg_stage2.val <= b_1_reg.val;
@@ -200,35 +144,10 @@ module goldschmidt (
     end
 
     // Stage 3
-    if (pipe_valid[2]) begin
+    if (pipe_valid[3]) begin
       x_1_reg.val <= tmp_x_1_stage3.val;
       y_1_reg.val <= tmp_y_1_stage3.val;
     end
-    /*
-      YY_1_reg.val <= tmp_YY_1_stage3.val;
-      b_1_reg_stage3.val <= b_1_reg_stage2.val;
-    end
-
-    // Stage 4 (Beginning of second iteration)
-    if (pipe_valid[3]) begin
-      x_1_reg_stage4.val <= x_1_reg.val;
-      y_1_reg_stage4.val <= y_1_reg.val;
-      b_2_reg.val <= tmp_b_2_stage4.val;
-    end
-
-    // Stage 5
-    if (pipe_valid[4]) begin
-      x_1_reg_stage5.val <= x_1_reg_stage4.val;
-      y_1_reg_stage5.val <= y_1_reg_stage4.val;
-      Y_2_reg.val <= tmp_Y_2_stage5.val;
-    end
-
-    // Stage 6
-    if (pipe_valid[5]) begin
-      x_2_reg.val <= tmp_x_2_stage6.val;
-      y_2_reg.val <= tmp_y_2_stage6.val;
-    end
-    */
 
   end
 
