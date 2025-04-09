e9d99d0deb948f253f3e07125fe6913aaa067906
hmelder <service@hugomelder.com>
2025-04-09 15:42:54 +0800

    Add auxiliary headers to tests

---
+++ hw/rt/tests/color.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "interval.h"
+#include "vec3.h"
+
+using color = vec3;
+
+// very simple gammar correction
+inline float linear_to_gamma(float linear_component) {
+  if (linear_component > 0) {
+    return std::sqrt(linear_component);
+  }
+
+  return 0;
+}
+
+struct rgb {
+  uint8_t r;
+  uint8_t g;
+  uint8_t b;
+};
+
+rgb get_rgb(const color &pixel_color) {
+  auto r = pixel_color.x();
+  auto g = pixel_color.y();
+  auto b = pixel_color.z();
+
+  // Apply a linear to gamma transform for gamma 2
+  r = linear_to_gamma(r);
+  g = linear_to_gamma(g);
+  b = linear_to_gamma(b);
+
+  // Translate the [0,1] component values to the byte range [0,255].
+  static const interval intensity(0.000f, 0.999f);
+  uint8_t rbyte = uint8_t(256 * intensity.clamp(r));
+  uint8_t gbyte = uint8_t(256 * intensity.clamp(g));
+  uint8_t bbyte = uint8_t(256 * intensity.clamp(b));
+
+  struct rgb s = {.r = rbyte, .g = gbyte, .b = bbyte};
+  return s;
+}
+
+void write_color(std::ostream &out, const color &pixel_color) {
+  auto p = get_rgb(pixel_color);
+  // Write out the pixel color components.
+  out << unsigned(p.r) << ' ' << unsigned(p.g) << ' ' << unsigned(p.b) << '\n';
+}
---
+++ hw/rt/tests/ray.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "vec3.h"
+
+class ray {
+public:
+  ray() {}
+
+  ray(const vec3 &origin, const vec3 &direction)
+      : orig(origin), dir(direction) {}
+
+  const vec3 &origin() const { return orig; }
+  const vec3 &direction() const { return dir; }
+
+  vec3 at(float t) const { return orig + t * dir; }
+
+private:
+  vec3 orig;
+  vec3 dir;
+};
